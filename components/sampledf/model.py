8721e6eeebac1dd50ebefee6769c587172074e46
Lucero <lual-99@hotmail.com>
2023-04-24 19:45:59 -0500

    fix paths

--- components/sampledf/model.py
+++ components/sampledf/model.py
@@ -3,10 +3,10 @@ from components.sampledf.functions import *
 
 
 #import dataframes
-df_mun = pd.read_excel('data\dfsample\speciesByMunicipality.xlsx', engine='openpyxl')
-df_consolidated = pd.read_excel('data\dfsample\consolidatedTableByYear.xlsx', engine='openpyxl')
-df_eb = pd.read_excel('data\dfsample\ebdSummaryVisits.xlsx', engine='openpyxl')
-clusters = pd.read_csv('data\dfsample\ClustersCSV.csv')
+df_mun = pd.read_excel('\data\dfsample\speciesByMunicipality.xlsx', engine='openpyxl')
+df_consolidated = pd.read_excel('\data\dfsample\consolidatedTableByYear.xlsx', engine='openpyxl')
+df_eb = pd.read_excel('\data\dfsample\ebdSummaryVisits.xlsx', engine='openpyxl')
+clusters = pd.read_csv('\data\dfsample\ClustersCSV.csv')
 
 #We update the dataframes to make some new we will use
 
